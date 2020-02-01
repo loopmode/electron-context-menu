26f36c4e6d9a2d709b57dfcbdee038d3fe190675
Jovica Aleksic <jovica.aleksic@xailabs.de>
2020-02-01 14:21:40 +0100

    Update fixture with a checkbox to enable/disable the context menu

--- fixture.js
+++ fixture.js
@@ -1,44 +1,6 @@
 'use strict';
 const path = require('path');
 const {app, BrowserWindow} = require('electron');
-const contextMenu = require('.');
-
-contextMenu({
-	labels: {
-		cut: 'Configured Cut',
-		copy: 'Configured Copy',
-		paste: 'Configured Paste',
-		save: 'Configured Save Image',
-		saveImageAs: 'Configured Save Image As…',
-		copyLink: 'Configured Copy Link',
-		inspect: 'Configured Inspect'
-	},
-	prepend: () => [
-		{
-			label: 'Unicorn'
-		},
-		{
-			type: 'separator'
-		},
-		{
-			type: 'separator'
-		},
-		{
-			label: 'Invisible',
-			visible: false
-		},
-		{
-			type: 'separator'
-		},
-		{
-			type: 'separator'
-		}
-	],
-	append: () => {},
-	showCopyImageAddress: true,
-	showSaveImageAs: true,
-	showInspectElement: false
-});
 
 (async () => {
 	await app.whenReady();
